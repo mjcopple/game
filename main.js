d076bcf0d18ce5f80e7f0da7b65d0aedeab764d2
Micah Copple <mjcopple@gmail.com>
2022-08-08 11:52:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var dollars = 0;
 var printers = 0;
+var trees = 0;
 
 function getDollars(number){
   dollars = dollars + number;
@@ -9,17 +10,30 @@ function getDollars(number){
 function buyPrinter(){
     var printerCost = Math.floor(10 * Math.pow(1.1,printers));     // works out the cost of this printers
     if(dollars >= printerCost){                                   // checks that the player can afford the printer
-        printers = printers + 1;                                   // increases number of printers
+      printers = printers + 1;                                   // increases number of printers
     	dollars = dollars - printerCost;                          // removes the dollars spent
-        document.getElementById('dollars').innerHTML = dollars;  // updates the number of dollars for the user
-        document.getElementById('printers').innerHTML = printers;  // updates the number of printers for the user
+      document.getElementById('dollars').innerHTML = dollars;  // updates the number of dollars for the user
+      document.getElementById('printers').innerHTML = printers;  // updates the number of printers for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,printers));       // works out the cost of the next printer
     document.getElementById('printerCost').innerHTML = nextCost;  // updates the printer cost for the user
 };
 
+function buyTree(){
+    var treeCost = Math.floor(100 * Math.pow(1.2,trees));
+    if(dollars >= treeCost){
+      trees = trees + 1;
+    	dollars = dollars - treeCost;
+      document.getElementById('dollars').innerHTML = dollars;
+      document.getElementById('trees').innerHTML = trees;
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.2,trees));
+    document.getElementById('treeCost').innerHTML = nextCost;
+};
+
 function mainLoop(){
   getDollars(printers);
+  getDollars(trees * 10);
 };
 
 window.setInterval(mainLoop, 1000);
